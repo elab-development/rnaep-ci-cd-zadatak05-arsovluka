2fa614b7c14c8cbcc5b9a2a105a5d776539f4565
Luka Arsov <arsovluka@gmail.com>
2026-05-23 00:54:12 +0200

    test_functional.py izmenjen

--- payment/tests/test_functional.py
+++ payment/tests/test_functional.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from unittest.mock import patch
+from redis_om import NotFoundError
 
 from payment.main import app
 
@@ -8,9 +9,8 @@ client = TestClient(app)
 
 
 def test_get_order_not_found():
-    with patch("payment.main.Order.get", side_effect=Exception()):
+    with patch("payment.main.Order.get", side_effect=NotFoundError):
         response = client.get("/orders/non-existing-id")
 
-    assert response.status_code in [404, 500]
-
-
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Order not found"
